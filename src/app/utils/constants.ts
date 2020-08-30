af12455a7ee72ee791dbb08b0fe97a3a5de4c928
Anujeet Chatterjee <canujeet@vmware.com>
2020-08-30 21:45:36 +0530

    removed node_env check

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,5 +1,2 @@
-// export const API_URL = "https://crypto-spring-boot.herokuapp.com";
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://getvaxxed.herokuapp.com/'
-    : 'http://localhost:8080';
+export const API_URL = 'https://getvaxxed.herokuapp.com';
+// export const API_URL = 'http://localhost:8080';
