4eb7447d9ac00347b07e20577aedf902dc95e7bf
Anujeet Chatterjee <canujeet@vmware.com>
2020-08-30 21:36:44 +0530

    node env from vercell

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,5 +1,5 @@
 // export const API_URL = "https://crypto-spring-boot.herokuapp.com";
 export const API_URL =
-  process.env['NODE_ENV'] === 'production'
+  process.env.NODE_ENV === 'production'
     ? 'https://getvaxxed.herokuapp.com/'
     : 'http://localhost:8080';
